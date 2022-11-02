b6ff9a9febc9b265a50a1baac607e083ae941626
ikerp <ikerp@192.168.1.136>
2022-11-02 15:17:50 +0100

    Metodos registro y Test carrito

--- src/Main/classes/Registro.java
+++ src/Main/classes/Registro.java
@@ -2,13 +2,55 @@ package Main.classes;
 
 public class Registro {
 
+	private String correoRegistro;
+	private Striing contrasenaRegistro;
+	
+	public Login( String contrasenaRegistro, String correoRegistro {
+		super();
+		this.contrasenaRegistro = contrasenaRegistro;
+		this.correoRegistro = correoRegistro;
+	}
+	
+	public Login() {
+		super();
+		this.contrasenaRegistro = "";
+		this.correoRegistro = "";
+	}
+	
+
+	public Login(Login l) {
+		super();
+		this.contrasenaRegistro = l.contrasenaRegistro;
+		this.correoRegistro = l.correoRegistro;
+	}
+	
+	
+	public String getCorreoRegistro() {
+		return correoRegistro;
+	}
+
+
+	public void setCorreoRegistro(String correoRegistro) {
+		this.correoRegistro = correoRegistro;
+	}
+
+
+
+	public String getContrasenaRegistro() {
+		return contrasenaRegistro;
+	}
+
+
+	public void setContrasenaRegistro(String contrasenaRegistro) {
+		this.contrasenaRegistro = contrasenaRegistro;
+	}
+
+	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 	
-	public void registrar() {
-		   
-	}
-
+	
 }
---
+++ src/Main/test/CarritoTest.java
@@ -0,0 +1,42 @@
+package Main.test;
+
+import static org.junit.Assert.assertEquals;
+
+import Main.classes.Carrito;
+
+public class CarritoTest {
+	private Carrito c;
+
+	//Construir objeto con atributos
+	@Before
+	public void setup() {
+		c = new Carrito(0,"nombre","fecha",30);
+	}
+	
+	//Test id del carrito
+	@Test
+	public void testGetSetId() {
+		assertEquals(0, c.getId());
+	}
+	
+	//Test nombre del carrito
+	@Test
+	public void testGetSetNombre() {
+		assertEquals("nombre",c.getNombre());
+	}
+	
+	//Test fecha del carrito
+	@Test
+	public void testGetSetFecha() {
+		assertEquals("fecha",c.getFecha());
+		}
+	
+	//Test nombre del carrito
+	@Test
+	public void testGetSetPrecio() {
+		assertEquals(30,c.getPrecio());
+		}
+		
+		
+	
+}
