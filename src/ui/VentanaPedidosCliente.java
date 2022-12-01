1dab89284daf1f12f2339e7fb71bc3bd11763fde
ignacioechavarren <ignacioechavarren@opendeusto.es>
2022-12-01 13:08:12 +0100

    Añadiendo mas ventanas

---
+++ src/ui/VentanaPedidosCliente.java
@@ -0,0 +1,10 @@
+package ui;
+
+public class VentanaPedidosCliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ui/VentanaPedidosRealizados.java
@@ -0,0 +1,10 @@
+package ui;
+
+public class VentanaPedidosRealizados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
