8ecac661e28c6eb6aeea214a90d3abb219840d1e
Yon Ander Escobal Fernández <73309512+yonan27@users.noreply.github.com>
2022-12-08 19:34:32 +0100

    Funcionalidad botones VentanInicio y VentanAdmin
    código realizado para poder ir entre ventanas mediante los botones creados

--- src/ui/VentanaAdmin.java
+++ src/ui/VentanaAdmin.java
@@ -2,17 +2,22 @@ package ui;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 public class VentanaAdmin extends JFrame{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private final Action actionBotonGestionStock = new botonGestionStock();
+	private final Action actionBotonGestionUsuarios = new botonGestionUsuarios();
+	private final Action actionBotonVerPedidos = new botonVerPedidos();
 
 	public VentanaAdmin() {
 		setResizable(false);
@@ -36,22 +41,20 @@ public class VentanaAdmin extends JFrame{
 		JButton btnGestionusuarios = new JButton("GESTIÓN DE USUARIOS");
 		btnGestionusuarios.setFont(new Font("Tahoma", Font.BOLD, 10));
 		btnGestionusuarios.setBounds(128, 113, 165, 21);
+		btnGestionusuarios.setAction(actionBotonGestionUsuarios);
 		contentPane.add(btnGestionusuarios);
 		
 		JButton btnGestionstock = new JButton("GESTIÓN DE STOCK");
 		btnGestionstock.setFont(new Font("Tahoma", Font.BOLD, 10));
 		btnGestionstock.setBounds(128, 71, 165, 21);
+		btnGestionstock.setAction(actionBotonGestionStock);
 		contentPane.add(btnGestionstock);
 		
 		JButton btnPedidosclientes = new JButton("PEDIDOS CLIENTES");
 		btnPedidosclientes.setFont(new Font("Tahoma", Font.BOLD, 10));
 		btnPedidosclientes.setBounds(128, 157, 165, 21);
+		btnPedidosclientes.setAction(actionBotonVerPedidos);
 		contentPane.add(btnPedidosclientes);
-		
-		JButton btnAtras = new JButton("ATRÁS");
-		btnAtras.setFont(new Font("Tahoma", Font.BOLD, 10));
-		btnAtras.setBounds(331, 218, 76, 21);
-		contentPane.add(btnAtras);
 		
 		JButton btnSalir = new JButton("SALIR");
 		btnSalir.setFont(new Font("Tahoma", Font.BOLD, 10));
@@ -61,6 +64,48 @@ public class VentanaAdmin extends JFrame{
 		
 		
 		};
+		
+		private class botonGestionStock extends AbstractAction {
+			public botonGestionStock() {
+				putValue(NAME, "GESTIÓN DE STOCK");
+				putValue(SHORT_DESCRIPTION, "Abrir la ventana para añadir o eliminar productos");
+			}
+
+			public void actionPerformed(ActionEvent e) {
+				VentanaGestionStock ventanaGestionStock = new VentanaGestionStock();
+				ventanaGestionStock.setVisible(true);
+				dispose();
+
+			}
+		}
+		
+		private class botonGestionUsuarios extends AbstractAction {
+			public botonGestionUsuarios() {
+				putValue(NAME, "GESTIÓN DE USUARIOS");
+				putValue(SHORT_DESCRIPTION, "Abrir la ventana para eliminar cuentas");
+			}
+
+			public void actionPerformed(ActionEvent e) {
+				VentanaGestionUsuarios ventanaGestionUsuarios = new VentanaGestionUsuarios();
+				ventanaGestionUsuarios.setVisible(true);
+				dispose();
+
+			}
+		}
+		
+		private class botonVerPedidos extends AbstractAction {
+			public botonVerPedidos() {
+				putValue(NAME, "PEDIDOS CLIENTES");
+				putValue(SHORT_DESCRIPTION, "Abrir la ventana para ver los pedidos realizados por los clientes");
+			}
+
+			public void actionPerformed(ActionEvent e) {
+				VentanaVerPedidosAdmin ventanaVerPedidosAdmin = new VentanaVerPedidosAdmin();
+				ventanaVerPedidosAdmin.setVisible(true);
+				dispose();
+
+			}
+		}
 			
 	  public static void main(String[] args) {
 	        VentanaAdmin vl = new VentanaAdmin();      // creamos una ventana
--- src/ui/VentanaInicio.java
+++ src/ui/VentanaInicio.java
@@ -1,5 +1,7 @@
 package ui;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -7,8 +9,8 @@ import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 
 
 public class VentanaInicio extends JFrame{
@@ -16,6 +18,11 @@ public class VentanaInicio extends JFrame{
 		
 		private static final long serialVersionUID = 1L;
 		private JPanel contentPane;
+		private final Action actionBotonInstrumentos = new botonInstrumentos();
+		private final Action actionBotonAltavoces = new botonAltavoces();
+		private final Action actionBotonDiscos = new botonDiscos();
+		private final Action actionBotonVerPedidos = new botonVerPedidos();
+	
 
 		
 		public VentanaInicio() {
@@ -37,28 +44,27 @@ public class VentanaInicio extends JFrame{
 			JButton btnComprarinstrumentos = new JButton("COMPRAR INSTRUMENTOS");
 			btnComprarinstrumentos.setFont(new Font("Tahoma", Font.BOLD, 10));
 			btnComprarinstrumentos.setBounds(136, 70, 171, 21);
+			btnComprarinstrumentos.setAction(actionBotonInstrumentos);
 			contentPane.add(btnComprarinstrumentos);
 			
 			JButton btnCompraraltavoces = new JButton("COMPRAR ALTAVOCES");
 			btnCompraraltavoces.setFont(new Font("Tahoma", Font.BOLD, 10));
 			btnCompraraltavoces.setBounds(136, 102, 171, 21);
+			btnCompraraltavoces.setAction(actionBotonAltavoces);
 			contentPane.add(btnCompraraltavoces);
 			
 			JButton btnComprardiscos = new JButton("COMPRAR DISCOS");
 			btnComprardiscos.setFont(new Font("Tahoma", Font.BOLD, 10));
 			btnComprardiscos.setBounds(136, 134, 171, 21);
+			btnComprardiscos.setAction(actionBotonDiscos);
 			contentPane.add(btnComprardiscos);
 			
 			JButton btnVerpedidos = new JButton("VER PEDIDOS");
 			btnVerpedidos.setFont(new Font("Tahoma", Font.BOLD, 10));
 			btnVerpedidos.setBounds(136, 166, 171, 21);
+			btnVerpedidos.setAction(actionBotonVerPedidos);
 			contentPane.add(btnVerpedidos);
 		
-			
-			JButton btnAtras = new JButton("ATRÁS");
-			btnAtras.setFont(new Font("Tahoma", Font.BOLD, 10));
-			btnAtras.setBounds(331, 218, 76, 21);
-			contentPane.add(btnAtras);
 			
 			JButton btnSalir = new JButton("SALIR");
 			btnSalir.setFont(new Font("Tahoma", Font.BOLD, 10));
@@ -67,6 +73,62 @@ public class VentanaInicio extends JFrame{
 			
 			
 			};
+			
+			private class botonInstrumentos extends AbstractAction {
+				public botonInstrumentos() {
+					putValue(NAME, "COMPRAR INSTRUMENTOS");
+					putValue(SHORT_DESCRIPTION, "Abrir la ventana para realizar pedido de instrumentos");
+				}
+
+				public void actionPerformed(ActionEvent e) {
+					VentanaInstrumentos ventanaInstrumentos = new VentanaInstrumentos();
+					ventanaInstrumentos.setVisible(true);
+					dispose();
+
+				}
+			}
+			
+			private class botonDiscos extends AbstractAction {
+				public botonDiscos() {
+					putValue(NAME, "COMPRAR DISCOS");
+					putValue(SHORT_DESCRIPTION, "Abrir la ventana para realizar pedido de discos");
+				}
+
+				public void actionPerformed(ActionEvent e) {
+					VentanaDiscos ventanaDiscos = new VentanaDiscos();
+					ventanaDiscos.setVisible(true);
+					dispose();
+
+				}
+			}
+			
+			private class botonAltavoces extends AbstractAction {
+				public botonAltavoces() {
+					putValue(NAME, "COMPRAR ALTAVOCES");
+					putValue(SHORT_DESCRIPTION, "Abrir la ventana para realizar pedido de altavoces");
+				}
+
+				public void actionPerformed(ActionEvent e) {
+					VentanaAltavoces ventanaAltavoces = new VentanaAltavoces();
+					ventanaAltavoces.setVisible(true);
+					dispose();
+
+				}
+			}
+			
+			private class botonVerPedidos extends AbstractAction {
+				public botonVerPedidos() {
+					putValue(NAME, "VER PEDIDOS");
+					putValue(SHORT_DESCRIPTION, "Abrir la ventana para ver los pedidos realizados");
+				}
+
+				public void actionPerformed(ActionEvent e) {
+					VentanaVerPedidos ventanaVerPedidos = new VentanaVerPedidos();
+					ventanaVerPedidos.setVisible(true);
+					dispose();
+
+				}
+			}
 				
 		  public static void main(String[] args) {
 		        VentanaInicio vl = new VentanaInicio();      // creamos una ventana
